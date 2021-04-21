d4487388a782cb753f71a542d4d94c2a05b5d45a
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2021-04-21 07:19:18 +0100

    setup app to run only with app.js and not .www

--- app.js
+++ app.js
@@ -67,4 +67,7 @@ db.on("open", ()=>{
   console.log("database running successfully");
 })
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+  console.log("App running on port: "+PORT);
+})
